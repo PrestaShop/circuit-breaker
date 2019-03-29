2b16e3e6d4dea1d5a2f79531dd8e55cf43e48c3d
Pablo Borowicz <pablo.borowicz@prestashop.com>
2019-03-29 12:30:31 +0100

    Downgrade Guzzle to v5 to allow this library to be compatible with PrestaShop 1.7

--- tests/CircuitBreakerTestCase.php
+++ tests/CircuitBreakerTestCase.php
@@ -2,13 +2,13 @@
 
 namespace Tests\PrestaShop\CircuitBreaker;
 
+use GuzzleHttp\Message\Request;
+use GuzzleHttp\Message\Response;
+use GuzzleHttp\Stream\Stream;
+use GuzzleHttp\Subscriber\Mock;
 use PrestaShop\CircuitBreaker\Clients\GuzzleClient;
 use GuzzleHttp\Exception\RequestException;
-use GuzzleHttp\Handler\MockHandler;
 use PHPUnit\Framework\TestCase;
-use GuzzleHttp\Psr7\Response;
-use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Psr7\Request;
 
 /**
  * Helper to get a fake Guzzle client.
@@ -23,14 +23,12 @@ abstract class CircuitBreakerTestCase extends TestCase
      */
     protected function getTestClient()
     {
-        $mock = new MockHandler([
+        $mock = new Mock([
             new RequestException('Service unavailable', new Request('GET', 'test')),
             new RequestException('Service unavailable', new Request('GET', 'test')),
-            new Response(200, [], '{"hello": "world"}'),
+            new Response(200, [], Stream::factory('{"hello": "world"}')),
         ]);
 
-        $handler = HandlerStack::create($mock);
-
-        return new GuzzleClient(['handler' => $handler]);
+        return new GuzzleClient(['mock' => $mock]);
     }
 }
